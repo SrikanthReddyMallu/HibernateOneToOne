75c02824e0da94ce49655a867384a63a46b11787
SrikanthReddyMallu <126140093+SrikanthReddyMallu@users.noreply.github.com>
2023-03-19 20:48:53 +0530

    Add files via upload

---
+++ getAllTheDetails.java
@@ -0,0 +1,17 @@
+package com.one;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class getAllTheDetails 
+{
+public static void main(String[] args) 
+{
+	EntityManagerFactory entityManagerFactory =Persistence.createEntityManagerFactory("vikas");
+	EntityManager entityManager = entityManagerFactory.createEntityManager();
+	EntityTransaction entityTransaction = entityManager.getTransaction();	
+
+}
+}
